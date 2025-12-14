9f1dd67af2f5aa9cbb7b4110bbab9a9176379567
admin-fatkat <79861506+admin-fatkat@users.noreply.github.com>
2025-12-14 22:31:51 +0900

    위치 섹션 버튼

--- src/pages/home/components/location.component.tsx
+++ src/pages/home/components/location.component.tsx
@@ -1,8 +1,13 @@
 import React from "react"
 import tw, { styled } from "twin.macro"
+import { CloseIcon } from "@/assets/icon"
+import wechatQrImg from "@/assets/images/wechat-qr.png"
 import GoogleMapComponent from "@/lib/components/google-map/google-map.component"
 import useLanguageValue from "@/lib/hooks/use-language-key"
 import { useTranslation } from "react-i18next"
+import useCustomNavigate from "@/lib/hooks/use-custom-navigate"
+import { Language } from "@/lib/locales/i18n.config"
+import Modal from "@/lib/components/modal/modal.component"
 
 const MapSection = tw.section`
   w-full bg-neutral overflow-hidden
@@ -62,8 +67,41 @@ const InfoTextWrapper = tw.div`
 
 const Location = () => {
   const tv = useLanguageValue()
-  const { t } = useTranslation()
+  const { t, i18n } = useTranslation()
+  const navigate = useCustomNavigate()
+  const language = i18n.language as Language
 
+  const [openWeChatModal, setOpenWeChatModal] = React.useState(false)
+
+  const handleChatClick = () => {
+    if (language === "ko" || language === "en") {
+      window.open("https://pf.kakao.com/_dxoiLn", "_blank")
+      return
+    }
+
+    if (language === "zh") {
+      // 중국어 → 위챗 모달 표시
+      setOpenWeChatModal(true)
+      return
+    }
+
+    if (language === "ja") {
+      window.open("https://line.me/R/ti/p/@235wfyao", "_blank")
+      return
+    }
+
+    if (language === "th") {
+      window.open(
+        "https://www.tiktok.com/@pecheclinic_th?is_from_webapp=1&sender_device=pc",
+        "_blank",
+      )
+      return
+    }
+
+    if (language === "zh-TW") {
+      window.open("https://line.me/R/ti/p/@683jgqmd", "_blank")
+    }
+  }
   return (
     <MapSection>
       <MapInner>
@@ -80,8 +118,13 @@ const Location = () => {
               </InfoTextWrapper>
 
               <ButtonGroup tw="mt-[1px]">
-                <SolidButton>{t("location.leftButton1")}</SolidButton>
-                <SolidButton>{t("location.leftButton2")}</SolidButton>
+                <SolidButton
+                  onClick={() => {
+                    navigate("/reservation/new")
+                  }}>
+                  {t("location.leftButton1")}
+                </SolidButton>
+                <SolidButton onClick={handleChatClick}>{t("location.leftButton2")}</SolidButton>
               </ButtonGroup>
             </InfoBlock>
 
@@ -94,8 +137,13 @@ const Location = () => {
               </InfoTextWrapper>
 
               <ButtonGroup>
-                <OutlineButton>{t("location.rightButton1")}</OutlineButton>
-                <OutlineButton>{t("location.rightButton2")}</OutlineButton>
+                <OutlineButton onClick={() => window.open("https://naver.me/FLe0V59M", "_blank")}>
+                  {t("location.rightButton1")}
+                </OutlineButton>
+                <OutlineButton
+                  onClick={() => window.open("https://kko.kakao.com/LK40uI5cBA", "_blank")}>
+                  {t("location.rightButton2")}
+                </OutlineButton>
               </ButtonGroup>
             </InfoBlock>
           </div>
@@ -108,6 +156,21 @@ const Location = () => {
           </GoogleMapWrapper>
         </MapColumn>
       </MapInner>
+      <Modal open={openWeChatModal} onClose={() => setOpenWeChatModal(false)} width="max-w-md">
+        <div tw="-mx-10 -my-8">
+          <div tw="bg-[#F3F3F3] w-full relative">
+            <div tw="px-4 pb-3 pt-12">
+              <div tw="text-[24px] font-time text-neutral90">Peche clinic</div>
+            </div>
+            <button tw="absolute top-3 right-4" onClick={() => setOpenWeChatModal(false)}>
+              <CloseIcon width={22} height={22} />
+            </button>
+          </div>
+          <div tw="p-6 flex justify-center bg-white">
+            <img src={wechatQrImg} alt="" tw="w-[240px] h-[240px] object-contain" />
+          </div>
+        </div>
+      </Modal>
     </MapSection>
   )
 }
